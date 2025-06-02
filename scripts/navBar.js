d58e400322cb8419d4e131660a2171f12a58b2cf
rudrakshJ <93859865+rudrakshJ@users.noreply.github.com>
2025-06-02 21:15:28 +0530

    Some changes made to navBar mainly to dropdown i.e. it dissappers if clicked outide + making navbar stick to the top of page once scrolled past it. Rest is kept the same for now. Adding anmiations would be too boring and much was learnt from the project.

--- scripts/navBar.js
+++ scripts/navBar.js
@@ -2,6 +2,12 @@ const toggleButton = document.querySelector('.nav-toggle-button');
 const dropDown = document.querySelector('.dropdown-menu');
 const toggleButtonIcon = document.querySelector('.nav-toggle-button i');
 
+// The below allows us to close navBars on phones when we click somewhere outside the dropdown
+const mainPart = document.getElementById('mainPart');
+const topPart = document.getElementById('topPart');
+const bottomPart = document.getElementById('bottomPart');
+
+
 toggleButton.onclick = () => {
        
 
@@ -12,3 +18,27 @@ toggleButton.onclick = () => {
     toggleButtonIcon.classList.add(isClose ? 'fa-bars' : 'fa-xmark');
 
 }
+
+
+mainPart.addEventListener('click', () => {
+    if (! dropDown.classList.contains('close')) {
+        dropDown.classList.add('close');
+        toggleButtonIcon.classList.remove('fa-xmark');
+        toggleButtonIcon.classList.add('fa-bars');
+    }
+});
+topPart.addEventListener('click', () => {
+    if (! dropDown.classList.contains('close')) {
+        dropDown.classList.add('close');
+        toggleButtonIcon.classList.remove('fa-xmark');
+        toggleButtonIcon.classList.add('fa-bars');
+    }
+});
+bottomPart.addEventListener('click', () => {
+    if (! dropDown.classList.contains('close')) {
+        dropDown.classList.add('close');
+        toggleButtonIcon.classList.remove('fa-xmark');
+        toggleButtonIcon.classList.add('fa-bars');
+    }
+});
+
